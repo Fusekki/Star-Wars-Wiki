eae4584ec9ae9f5cf9603661d439b3c7a10e4acb
grimal <phillip@philliprstafford.com>
2017-02-10 13:12:29 -0500

    Add parseService.  Create function in parseService to parse categories with URLs and make their respective API calls.  Store result in array that is watched via controller. Create array with categories that have these URLs.  Interpolate those results in the template.

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -45,58 +45,31 @@ angular.module('swApp')
     })
 
     // This is the controller for the People results
-    .controller('peopleCtrl', function ($scope, searchService, logicService, apiService) {
-
-        var apiResults = [];
-        var hw = null;
-        var f_array = [];
-
-        var returned_results = [];
-        $scope.films = [];
+    .controller('peopleCtrl', function ($scope, searchService, logicService, apiService, parseService) {
 
         console.log('in people controller.');
 
-
-
+        var parsed_objects = [];
 
         $scope.search_term = searchService.search_term;
 
         $scope.category = searchService.category;
 
 
-        console.log($scope.search_term);
-        console.log($scope.category);
+        apiService.getData(function(response) {
+            $scope.core_results = response.data.results;
+            parseService.parseResults($scope.core_results);
+        }, function(err) {
+            console.log(err.status);
+        });
 
-        if (!logicService.getCacheItems($scope.search_term)) {
-            apiService.getData(function(response) {
-                var trimmed_results = response.data.results;
-                // Trimmed_results contains an object for each separate item returned.
-                trimmed_results.forEach(function(obj){
-                    parseHwUrl(obj);
-                    obj.homeworld = hw;
+        $scope.$watch('films', function () {
+            $scope.films = parseService.films;
+        });
 
-                    populateFArray(obj);
-
-                    apiResults.push(obj);
-                });
-
-                $scope.item = apiResults;
-                logicService.setCacheItems($scope.search_term, apiResults);
-
-
-                console.log($scope.item.length);
-
-                console.log(response);
-            }, function(err) {
-                console.log(err.status);
-            });
-        } else {
-            $scope.item = logicService.getCacheItems($scope.search_term);
-            populateHwArray($scope.item);
-            populateFArray($scope.item);
-        }
-
-
+        $scope.$watch('homeworlds', function () {
+            $scope.homeworlds = parseService.homeworlds;
+        });
 
         $scope.convertToLocal = function(some_date) {
             return logicService.localizeThis(some_date);
@@ -104,37 +77,6 @@ angular.module('swApp')
 
         $scope.convertWeight = function(mass) {
             return logicService.weightThis(mass);
-        };
-
-        var parseHwUrl = function(obj) {
-{
-            console.log(obj.homeworld);
-            apiService.getDataUrl(obj.homeworld, function(response) {
-
-                // $scope.homeworld = response.data.name;
-                console.log(response);
-                hw = response.data.name;
-            }, function(err) {
-                console.log(err.status);
-            });
-
-
-        };
-
-        var populateFArray = function(items_returned) {
-            items_returned.forEach(function(item) {
-                console.log(item.films);
-                item.films.forEach(function(film_url) {
-                    apiService.getDataUrl(film_url, function(response) {
-                        $scope.films.push(response.data.title);
-                        console.log(response);
-                    }, function(err) {
-                        console.log(err.status);
-                    });
-                });
-
-            })
-
         };
 
 
--- services/services.js
+++ services/services.js
@@ -37,7 +37,7 @@ angular.module('swApp')
             return myCache.get(cache);
         };
 
-        var setCacheStatus = function (cache, items) {
+        var setCacheItem = function (cache, items) {
             myCache.put(cache, items);
         };
 
@@ -45,9 +45,8 @@ angular.module('swApp')
 
         return {
 
-            setCacheItems: function(cacheName, cacheContents) {
-
-
+            setCacheItems: function(name, contents) {
+                setCacheItem(name, contents);
             },
 
             getCacheItems: function(cacheName) {
@@ -87,8 +86,8 @@ angular.module('swApp')
         self.category = logicService.lowerCaseThis(logicService.category);
         self.search_term = logicService.search_term;
 
-        console.log(self.search_term);
-        console.log(self.category);
+        // console.log(self.search_term);
+        // console.log(self.category);
 
         apiService.category = logicService.lowerCaseThis(self.category);
         apiService.search_term = self.search_term;
@@ -101,8 +100,8 @@ angular.module('swApp')
 
         self.category = logicService.lowerCaseThis(logicService.category);
         self.search_term = logicService.search_term;
-        console.log(self.search_term);
-        console.log(self.category);
+        // console.log(self.search_term);
+        // console.log(self.category);
 
         console.log('in api service');
 
@@ -122,4 +121,95 @@ angular.module('swApp')
             return url + "1";
         }
 
+    })
+
+    .service('parseService', function (apiService, logicService) {
+        var self = this;
+        console.log('in parseService');
+
+        var categories_with_url = ["homeworld"];
+
+        var categories_with_array = ["films"];
+        self.homeworlds = [];
+        self.films = [];
+
+        // whichever category the user specifies, there may be multiple results.
+        // First, let's create the API call and then store all results in some array.
+
+        // Call API, push results to array
+
+        // For each result, we need to check for specific fields that require additional API calls.
+        // These categories should be stored in an object.
+
+        // Once the call is completed, store the URLName in the cache with the returned info.
+
+        // If the category is an array object (such as films), push those items to a subarray.
+        // Otherwise, just push to a variable.
+        // Cycle through each item in subarray, call the API, store the URL into the cache.
+        // After, push the result to subarray.
+
+        // After all calls are done for one object, create a new $scope object that contains the results
+        // (both from original, and include new variable(s) with the results of those API calls as well as arrays.
+
+        self.parseResults = function(results_to_parse) {
+
+            // We need to cycle through object returned in the event there are many.
+            results_to_parse.forEach(function(result_item) {
+                var film_array = [];
+
+                // For each result, we need to check for specific fields that require additional API calls.
+                // These categories should be stored in an object.
+                categories_with_url.forEach(function(category) {
+                    if (result_item[category]) {
+                        // make api call
+                        var url = result_item[category];
+                        console.log(url);
+                        apiService.getDataUrl(url, function(response) {
+                            // Store the name for the resolved URL in a variable.
+                            var homeworld_name = response.data.name;
+                            self.homeworlds.push(homeworld_name);
+                            // Push the URL and result to the Cache
+                            logicService.setCacheItems(url, response);
+                            }, function(err) {
+                                console.log(err.status);
+                            });
+                        }
+                });
+
+                categories_with_array.forEach(function(category) {
+                    if (result_item[category]) {
+                        // make api call
+                        var some_url = result_item[category];
+                        console.log(some_url);
+                        some_url.forEach(function(url) {
+                            console.log(url);
+                            apiService.getDataUrl(url, function(response) {
+                                // Store the name for the resolved URL in a variable.
+                                console.log(category);
+                                switch (category) {
+                                    case 'films':
+                                        console.log('they are films');
+                                        var film_name = response.data.title;
+                                        console.log(film_name);
+
+                                        film_array.push(film_name);
+                                        // self.films.push(film_name);
+                                }
+                                // Push the URL and result to the Cache
+                                logicService.setCacheItems(url, response);
+                                console.log(logicService.getCacheItems(url));
+                            }, function(err) {
+                                console.log(err.status);
+                            });
+                        });
+                    }
+                });
+                self.films.push(film_array);
+            });
+
+
+            console.log(self.films);
+            console.log(self.homeworlds);
+
+        }
     })
