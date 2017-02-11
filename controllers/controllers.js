4e6abbbac85ea6248a0f5fa702052be78ac837f2
grimal <phillip@philliprstafford.com>
2017-02-11 14:35:28 -0500

    Add cacheFactory functions prior to API calls.  Need to fix the homeworld and films arrays when multiple results are returned from search.

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -39,6 +39,7 @@ angular.module('swApp')
             if ($scope.category) {
                 var pathCategory = logicService.lowerCaseThis($scope.category);
                 $location.path("/" + pathCategory);
+                console.log($scope.search_term);
             }
 
         };
@@ -47,21 +48,43 @@ angular.module('swApp')
     // This is the controller for the People results
     .controller('peopleCtrl', function ($scope, searchService, logicService, apiService, parseService) {
 
+        var self = this;
+        self.cache_results = null;
+
+        $scope.search_term = logicService.search_term;
+
         console.log('in people controller.');
 
-        var parsed_objects = [];
-
-        $scope.search_term = searchService.search_term;
-
-        $scope.category = searchService.category;
+        apiService.search_term = $scope.search_term;
+        apiService.category = logicService.lowerCaseThis(logicService.category);
 
 
-        apiService.getData(function(response) {
-            $scope.core_results = response.data.results;
+
+
+        self.cache_results = logicService.getCacheItem($scope.search_term);
+        console.log(self.cache_results);
+
+        // If the cache item does not exist, make the API call.
+        if (!self.cache_results) {
+            console.log('cache doesnt have item. making api call.');
+            apiService.getData(function(response) {
+                $scope.core_results = response.data.results;
+                console.log($scope.core_results);
+                // Cache the item in its orginal form
+                // console.log('caching original item before parse.');
+                logicService.setCacheItem($scope.search_term, $scope.core_results);
+                parseService.parseResults($scope.core_results);
+            }, function(err) {
+                console.log(err.status);
+            });
+        } else {
+            console.log('item is cached.  retrieving values from cache.');
+            $scope.core_results = cache_results;
             parseService.parseResults($scope.core_results);
-        }, function(err) {
-            console.log(err.status);
-        });
+        }
+
+
+
 
         $scope.$watch('films', function () {
             $scope.films = parseService.films;
--- services/services.js
+++ services/services.js
@@ -33,24 +33,31 @@ angular.module('swApp')
             return lbs + " lbs " + oz +  " oz";
         };
 
-        var getCacheStatus = function (cache) {
-            return myCache.get(cache);
+        var getCacheItem = function (item) {
+            return myCache.get(item);
         };
 
         var setCacheItem = function (cache, items) {
             myCache.put(cache, items);
         };
 
+
         // Public variables
 
         return {
 
-            setCacheItems: function(name, contents) {
+            showCacheLengthWrapper: function() {
+                var info = myCache.info();
+                return info.size;
+            },
+
+            setCacheItem: function(name, contents) {
                 setCacheItem(name, contents);
             },
 
-            getCacheItems: function(cacheName) {
-                return myCache.get(cacheName);
+            getCacheItem: function(cacheName) {
+                // console.log(cacheName);
+                return getCacheItem(cacheName);
             },
 
             getCategories: function() {
@@ -83,6 +90,8 @@ angular.module('swApp')
         console.log('in search service');
         var self=this;
 
+        console.log(logicService.search_term);
+
         self.category = logicService.lowerCaseThis(logicService.category);
         self.search_term = logicService.search_term;
 
@@ -96,18 +105,19 @@ angular.module('swApp')
 
     .service('apiService', function($http, logicService) {
 
+        console.log('in api service');
         var self = this;
 
         self.category = logicService.lowerCaseThis(logicService.category);
         self.search_term = logicService.search_term;
-        // console.log(self.search_term);
+        console.log(self.search_term);
         // console.log(self.category);
 
-        console.log('in api service');
+
 
 
         this.getData = function(callback, err) {
-            $http.get('https://swapi.co/api/' + this.category + '/?search='+ this.search_term)
+            $http.get('https://swapi.co/api/' + self.category + '/?search='+ self.search_term)
                  .then(callback,err);
         }
 
@@ -117,7 +127,7 @@ angular.module('swApp')
         }
 
         this.test = function(url) {
-            console.log(url);
+            // console.log(url);
             return url + "1";
         }
 
@@ -127,11 +137,14 @@ angular.module('swApp')
         var self = this;
         console.log('in parseService');
 
-        var categories_with_url = ["homeworld"];
+        self.categories_with_url = ["homeworld"];
 
-        var categories_with_array = ["films"];
-        self.homeworlds = [];
+        self.categories_with_array = ["films"];
+
+        self.film_array = [];
         self.films = [];
+        self.homeworlds = [];
+
 
         // whichever category the user specifies, there may be multiple results.
         // First, let's create the API call and then store all results in some array.
@@ -153,63 +166,130 @@ angular.module('swApp')
 
         self.parseResults = function(results_to_parse) {
 
+            // var film_array = [];
+
+            if (self.film_array) {
+                console.log('self.film_array has data. clearing.');
+                self.film_array = [];
+            }
+
+
+            if (self.homeworlds.length) {
+                console.log('self.homeworlds has data. clearing.');
+                self.homeworlds = [];
+            }
+
+            if (self.films.length) {
+                console.log('self.films has data. clearing.');
+                self.films = [];
+
+            }
+
+
+            // console.log(film_array);
+
             // We need to cycle through object returned in the event there are many.
             results_to_parse.forEach(function(result_item) {
-                var film_array = [];
+
 
                 // For each result, we need to check for specific fields that require additional API calls.
                 // These categories should be stored in an object.
-                categories_with_url.forEach(function(category) {
+
+                // For each homeworld, for example
+                self.categories_with_url.forEach(function(category) {
+                    console.log(category);
                     if (result_item[category]) {
                         // make api call
                         var url = result_item[category];
-                        console.log(url);
-                        apiService.getDataUrl(url, function(response) {
-                            // Store the name for the resolved URL in a variable.
-                            var homeworld_name = response.data.name;
-                            self.homeworlds.push(homeworld_name);
-                            // Push the URL and result to the Cache
-                            logicService.setCacheItems(url, response);
+                        // console.log(url);
+                        // Before any API calls, we need to check the cache.
+                        var cache_results = logicService.getCacheItem(url);
+                        // console.log(cache_results);
+                        if (!cache_results) {
+                            apiService.getDataUrl(url, function(response) {
+                                console.log('homeworld is not in cache');
+                                // Store the name for the resolved URL in a variable.
+                                var homeworld_name = response.data.name;
+                                self.homeworlds.push(homeworld_name);
+                                // Push the URL and result to the Cache
+                                logicService.setCacheItem(url, response);
+
                             }, function(err) {
                                 console.log(err.status);
                             });
+                        } else {
+                            console.log('homeworld is in cache');
+                            // console.log(cache_results);
+                            var homeworld_name = cache_results.data.name;
+                            // console.log(homeworld_name);
+                            self.homeworlds.push(homeworld_name);
                         }
+                        // console.log(self.homeworlds);
+                    }
                 });
 
-                categories_with_array.forEach(function(category) {
+                // Like films, for example
+                self.categories_with_array.forEach(function(category) {
+                    // console.log(category);
                     if (result_item[category]) {
                         // make api call
                         var some_url = result_item[category];
-                        console.log(some_url);
+                        // console.log(some_url);
+
+                        // For each film in the results
+
                         some_url.forEach(function(url) {
-                            console.log(url);
-                            apiService.getDataUrl(url, function(response) {
-                                // Store the name for the resolved URL in a variable.
-                                console.log(category);
+                            // console.log(url);
+                            var cache_results = logicService.getCacheItem(url);
+                            if (!cache_results) {
+                                apiService.getDataUrl(url, function(response) {
+                                    console.log('films is not in cache');
+                                    // Store the name for the resolved URL in a variable.
+                                    // console.log(category);
+                                    switch (category) {
+                                        case 'films':
+                                            // console.log('they are films');
+                                            var film_name = response.data.title;
+                                            // console.log(film_name);
+
+                                            self.film_array.push(film_name);
+                                        // self.films.push(film_name);
+                                    }
+                                    // Push the URL and result to the Cache
+                                    logicService.setCacheItem(url, response);
+                                    // console.log(logicService.getCacheItem(url));
+
+                                }, function(err) {
+                                    console.log(err.status);
+                                });
+                            } else {
+                                // console.log(category);
+                                console.log('films are in cache');
                                 switch (category) {
                                     case 'films':
-                                        console.log('they are films');
-                                        var film_name = response.data.title;
-                                        console.log(film_name);
+                                        // console.log('they are films');
+                                        // console.log(cache_results);
+                                        var film_name = cache_results.data.title;
+                                        // console.log(film_name);
 
-                                        film_array.push(film_name);
-                                        // self.films.push(film_name);
+                                        self.film_array.push(film_name);
+                                    // self.films.push(film_name);
                                 }
-                                // Push the URL and result to the Cache
-                                logicService.setCacheItems(url, response);
-                                console.log(logicService.getCacheItems(url));
-                            }, function(err) {
-                                console.log(err.status);
-                            });
+
+                            }
+                            // console.log(self.films);
                         });
+
+                        // self.films.push(self.film_array);
                     }
                 });
-                self.films.push(film_array);
+                self.films.push(self.film_array);
+                // console.log(self.films);
             });
 
 
-            console.log(self.films);
-            console.log(self.homeworlds);
+            // console.log(self.films);
+            // console.log(self.homeworlds);
 
         }
     })
